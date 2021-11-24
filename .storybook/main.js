f17fd8b4f5db9a704a02fd594361e92e4dfd4195
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2021-11-24 12:03:41 +0100

    chore: regenerate package-lock.json (#301)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,10 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
   stories: [],
   addons: ['@storybook/addon-docs', '@storybook/addon-essentials', '@storybook/addon-a11y'],
+  webpackFinal: (config) => {
+    config.resolve.plugins = [...(config.resolve.plugins || []), new TsconfigPathsPlugin({ extensions: config.resolve.extensions })];
+    return config;
+  },
 };
