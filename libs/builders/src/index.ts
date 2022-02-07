d960b2b9115d6220b6d5f29b06d2873358f5ef93
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2022-02-07 19:20:22 +0100

    refactor(builders): :recycle: create unique schema for project builder

--- libs/builders/src/index.ts
+++ libs/builders/src/index.ts
@@ -2,4 +2,4 @@ export * as commandBuilder from './command';
 export * from './core';
 export * as faviconsBuilder from './favicons';
 export * as imageOptimizerBuilder from './image-optimizer';
-export * as semanticReleaseBuilder from './semantic-release';
+export * from './semantic-release';
