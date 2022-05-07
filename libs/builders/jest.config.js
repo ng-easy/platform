ee8bb00296f16b37c25b847f37a5c3235be1b3eb
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2022-05-07 16:05:01 +0200

    chore(builders): :white_check_mark: add codecov to check coverage
    Closes #407

--- libs/builders/jest.config.js
+++ libs/builders/jest.config.js
@@ -8,6 +8,7 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageReporters: ['lcov'],
   coverageDirectory: '../../coverage/libs/builders',
   testEnvironment: 'node',
 };
