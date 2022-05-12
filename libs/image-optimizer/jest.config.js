2b5fa7949492a62e5c8376bca432e275a3de73b6
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2022-05-12 08:53:48 +0200

    test(*): :test_tube: add test coverage for all projects (#498)

--- libs/image-optimizer/jest.config.js
+++ libs/image-optimizer/jest.config.js
@@ -2,9 +2,7 @@ module.exports = {
   displayName: 'image-optimizer',
   preset: '../../jest.preset.js',
   globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
+    'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' },
   },
   testEnvironment: 'node',
   transform: {
@@ -12,4 +10,5 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/image-optimizer',
+  coverageReporters: ['lcov'],
 };
