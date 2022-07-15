a9385053a40d0be279ebb0460e53b433277acd97
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2022-07-15 17:07:23 +0200

    fix(builders): :bug: fix import of execa since it is an ESM module (#616)

--- libs/builders/src/semantic-release/lib/git.ts
+++ libs/builders/src/semantic-release/lib/git.ts
@@ -1,21 +1,25 @@
-import { execa } from 'execa';
+import { importEsm } from '../../core';
 
 export async function getGitCurrentSha(): Promise<string> {
+  const { execa } = await importEsm('execa');
   const { stdout } = await execa('git', ['log', '-n', '1']);
   return stdout;
 }
 
 export async function getGitRemoteHeadSha(): Promise<string> {
+  const { execa } = await importEsm('execa');
   const { stdout } = await execa('git', ['log', '-n', '1', 'origin/HEAD']);
   return stdout;
 }
 
 export async function getGitStatus(): Promise<string> {
+  const { execa } = await importEsm('execa');
   const { stdout } = await execa('git', ['status']);
   return stdout;
 }
 
 export async function getGitPullRebase(): Promise<string> {
+  const { execa } = await importEsm('execa');
   await execa('git', ['fetch', 'origin', 'HEAD']);
   const { stdout } = await execa('git', ['pull', '--rebase']);
   return stdout;
