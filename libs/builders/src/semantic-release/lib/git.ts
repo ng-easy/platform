558f671b703d23ee70786285b518d5a881488a52
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2022-07-16 11:16:30 +0200

    fix(builders): :bug: fix getting remote commit (#599)

--- libs/builders/src/semantic-release/lib/git.ts
+++ libs/builders/src/semantic-release/lib/git.ts
@@ -8,7 +8,9 @@ export async function getGitCurrentSha(): Promise<string> {
 
 export async function getGitRemoteHeadSha(): Promise<string> {
   const { execa } = await importEsm('execa');
-  const { stdout } = await execa('git', ['rev-parse', 'origin/HEAD']);
+  const repoUrl = await getGitRepoUrl();
+  const currentBranch = await getGitCurrentBranch();
+  const { stdout } = await execa('git', ['ls-remote', '--heads', repoUrl, currentBranch]);
   return stdout;
 }
 
@@ -23,4 +25,16 @@ export async function getGitPullRebase(): Promise<string> {
   const { stdout: outFetch } = await execa('git', ['fetch']);
   const { stdout: outRebase } = await execa('git', ['pull', '--rebase']);
   return `${outFetch}\n${outRebase}`;
+}
+
+export async function getGitRepoUrl(): Promise<string> {
+  const { execa } = await importEsm('execa');
+  const { stdout } = await execa('git', ['config', '--get', 'remote.origin.url']);
+  return stdout;
+}
+
+export async function getGitCurrentBranch(): Promise<string> {
+  const { execa } = await importEsm('execa');
+  const { stdout } = await execa('git', ['rev-parse', '--abbrev-ref', 'HEAD']);
+  return stdout;
 }
